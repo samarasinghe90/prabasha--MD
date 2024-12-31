2d72506ed22edd9f1d10d1e93290c7c52896ca52
samarasinghe90 <sithunprabasha988@gmail.com>
2024-12-31 20:13:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const ownerNumber = ['94704519149']
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
-if(!config.SESSION_ID) return console.log('Please add your session to SESSION_ID env !!')
+if(!config.SESSION_ID) return console.log('Bb/A+9nNuaaFJIlSQg98zFrVPS9lUKCR0XWI8xLAFCUO')
 const sessdata = config.SESSION_ID
 const filer = File.fromURL(`https://mega.nz/file/${sessdata}`)
 filer.download((err, data) => {
